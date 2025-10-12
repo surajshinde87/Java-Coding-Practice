764ca98bd27708861d8aecdacff6797267e80c40
Suraj Shinde <surajpshinde8767@gmail.com>
2025-10-12 22:06:19 +0530

    Check if the string is pangram

---
+++ Q30-pangram_string/Main.java
@@ -0,0 +1,22 @@
+// Check if a string is a pangram
+// A pangram is a sentence that contains every letter of the alphabet at least once.
+public class Main {
+
+    public static void main(String[] args) {
+       
+        // String str = "The quick brown fox jumps over the lazy dog";  // true
+        String str = "Hello World";    // false
+
+        System.out.println(isPangram(str) ? "Pangram" : "Not a Pangram");
+
+    }
+    public static boolean isPangram(String str){
+        str = str.toLowerCase();
+        for(char ch = 'a'; ch <= 'z'; ch++){
+            if(str.indexOf(ch) < 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
